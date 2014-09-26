895e8999286c5e1b8bdabb858979db866a8b425e
Jennifer Fong <jen@ednapiranha.com>
2014-09-26 09:20:25 -0700

    this is temporary.

--- public/js/music.js
+++ public/js/music.js
@@ -1,15 +1,8 @@
 var $ = require('jquery');
-
-var music = [
-  'https://dl.dropboxusercontent.com/u/17064414/champagne.wav',
-  'https://dl.dropboxusercontent.com/u/17064414/feelin-u.wav',
-  'https://dl.dropboxusercontent.com/u/17064414/funk.wav',
-  'https://dl.dropboxusercontent.com/u/17064414/junkyard.wav',
-  'https://dl.dropboxusercontent.com/u/17064414/meatspace.wav',
-  'https://dl.dropboxusercontent.com/u/17064414/pleasure.wav'
-];
+//var musicArr = require('https://dl.dropboxusercontent.com/u/1913694/128382random');
 
 var audioTag = null;
+var music = ['https://dl.dropboxusercontent.com/u/1913694/Uia49tkWqAO1.128.mp3'];
 
 var toggleMusic = function () {
   var musicEl = $('#music');
@@ -29,10 +22,12 @@ var getSong = function () {
   var audio = music[Math.floor(Math.random() * music.length)];
   var vaudioTag = document.createElement('audio');
   vaudioTag.src = audio;
-  vaudioTag.addEventListener('ended', function(){
+
+  vaudioTag.addEventListener('ended', function () {
     audioTag = null;
     toggleMusic();
   });
+
   return vaudioTag;
 };
 
