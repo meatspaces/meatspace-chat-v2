18622514e517570bcec1248b27cdf7689d5f8437
Jory Graham <jory.graham@gmail.com>
2014-11-17 00:14:47 -0500

    Do less unnecessary work!
    Squished!

--- public/js/services.js
+++ public/js/services.js
@@ -31,75 +31,80 @@ exports.getMessage = function (data, mutedFP, userIdManager, profile, messages) 
     window.ga('send', 'event', 'message', 'receive');
   }
 
-  if (!mutedFP[data.fingerprint]) {
-    var li = $('<li data-fp="' + data.fingerprint + '" />');
-    var videoContainer = $('<div class="video-container"/>');
-    var video = $('<video src="' + data.media + '" autoplay="autoplay" loop />');
-    var convertButton = $('<a class="convert">Save as GIF</a>');
-    var p = $('<p />');
-    var userControls = '';
+  if (mutedFP[data.fingerprint]) {
+    return;
+  }
 
-    if (!userIdManager.contains(data.fingerprint)) {
-      userControls = '<button class="mute">mute</button>';
-    }
+  var children = messages.children();
+  var length = children.length;
 
-    var created = moment(new Date(data.created));
-    var time = $('<time datetime="' + created.toISOString() + '" class="timestamp">' +
-      created.format('LT') + '</time>');
-
-    var actions = $('<div class="actions">' + userControls +'</div>');
-    p.html(data.message);
-    videoContainer.append(video).append(convertButton);
-    li.append(videoContainer).append(p).append(actions);
-    messages.append(li);
-    p.append(time);
-
-    var children = messages.find('li');
-
-    var waypoints = [];
-
-    waypoints.push(new Waypoint({
-      element: li[0],
-      handler: function (direction) {
-        $(this.element).toggleClass('in-view', direction === 'up');
-        video[0][direction === 'up' ? 'play' : 'pause']();
-      },
-      offset: function () {
-        return -$(this.element).outerHeight();
-      }
-    }));
-
-    waypoints.push(new Waypoint({
-      element: li[0],
-      handler: function (direction) {
-        $(this.element).toggleClass('in-view', direction === 'down');
-        video[0][direction === 'down' ? 'play' : 'pause']();
-      },
-      offset: '100%'
-    }));
-
-    li.data('waypoints', waypoints);
-
+  if (length) {
+    var last = children.get(-1);
     var size = $(window).innerHeight();
-    var last = messages[0].lastChild;
     var bottom = last ? last.getBoundingClientRect().bottom : 0;
     var follow = bottom < size + 50;
 
     if (follow) {
-      if (children.length > MAX_LIMIT) {
-        children.slice(0, children.length - MAX_LIMIT).each(function () {
+      if (length > MAX_LIMIT) {
+        children.slice(0, length - MAX_LIMIT).each(function () {
           $(this).data('waypoints').forEach(function (waypoint) {
             waypoint.destroy();
           });
+
           var saveLink = $(this).find('.save');
           if (saveLink.length) {
             window.URL.revokeObjectURL(saveLink.attr('href'));
           }
         }).remove();
-        Waypoint.refreshAll();
       }
-
-      li[0].scrollIntoView();
     }
+  }
+
+  var userControls = '';
+  if (!userIdManager.contains(data.fingerprint)) {
+    userControls = '<button class="mute">mute</button>';
+  }
+
+  var created = moment(new Date(data.created));
+
+  var timestamp =
+        '<time datetime="' + created.toISOString() + '" class="timestamp">' +
+          created.format('LT') + '</time>';
+
+  var markup =
+        '<li data-fp="' + data.fingerprint + '">' +
+          '<div class="video-container">' +
+            '<video src="' + data.media + '" autoplay="autoplay" loop />' +
+            '<a class="convert">Save as GIF</a>' +
+          '</div>' +
+          '<p>' + data.message + timestamp + '</p>' +
+          '<div class="actions">' + userControls +'</div>' +
+        '</li>';
+
+  var li = $(markup);
+  var eLi = li[0];
+
+  messages.append(li);
+
+  var opts = function (refDir, offsetFn) {
+    return {
+      element: eLi,
+      offset: offsetFn,
+      handler: function (direction) {
+        $(this.element).toggleClass('in-view', direction === refDir);
+        li.find('video')[0][direction === refDir ? 'play' : 'pause']();
+      }
+    };
+  };
+
+  li.data('waypoints', [
+    new Waypoint(opts('up', function () {return -$(this.element).outerHeight();})),
+    new Waypoint(opts('down', '100%'))
+  ]);
+
+  Waypoint.refreshAll();
+
+  if (follow) {
+    eLi.scrollIntoView();
   }
 };
